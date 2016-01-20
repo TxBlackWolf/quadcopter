4ea2dbf488096266e4d6aa26262a9313abfce10d
Kuba Sejdak <jakub.sejdak@phoesys.com>
2016-01-20 09:29:28 +0100

    Added draft of doxyfile. Added parsing emulator commands.

--- common/commands/emulator.c
+++ common/commands/emulator.c
@@ -12,8 +12,34 @@
 
 #include "emulator.h"
 
+#include <stdio.h>
+
+static CommandCallback_t command_callbacks[EMULATED_DEVICE_COUNT];
+
+bool command_registerEmulatorCallback(EmulatedDevice_t device, CommandCallback_t callback)
+{
+	if(command_callbacks[device])
+		return false;
+
+	command_callbacks[device] = callback;
+	return true;
+}
+
+bool command_unregisterEmulatorCallback(EmulatedDevice_t device)
+{
+	if(!command_callbacks[device])
+		return false;
+
+	command_callbacks[device] = NULL;
+	return true;
+}
+
 bool command_parseEmulator(uint8_t *payload, uint32_t size)
 {
-    // TODO: implement.
+	EmulatorCommand_t *command = (EmulatorCommand_t *) payload;
+	if(!command_callbacks[command->device])
+		return false;
+
+	command_callbacks[command->device](payload, size);
     return true;
 }
--- common/commands/emulator.h
+++ common/commands/emulator.h
@@ -18,14 +18,26 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+typedef void (*CommandCallback_t)(uint8_t *, uint32_t);
+
 typedef enum {
-    EMULATED_DEVICE_GPIO
+    EMULATED_DEVICE_GPIO,
+	EMULATED_DEVICE_COUNT
 } EmulatedDevice_t;
 
 typedef struct {
     EmulatedDevice_t device;
 } __attribute__((packed)) EmulatorCommand_t;
 
+typedef struct {
+	uint32_t port;
+	uint32_t pin;
+	uint32_t value;
+	uint32_t name_size;
+} __attribute__((packed)) EmulatorCommandGPIO_t;
+
+bool command_registerEmulatorCallback(EmulatedDevice_t device, CommandCallback_t callback);
+bool command_unregisterEmulatorCallback(EmulatedDevice_t device);
 bool command_parseEmulator(uint8_t *payload, uint32_t size);
 
 #endif
